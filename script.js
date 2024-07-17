0ebff3357beaf333d59e46d1e2a8cde2b437df97
Aarish <mohdaarish127@gmail.com>
2024-07-17 15:28:34 +0530

    date

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const API_KEY="Your Api Key";
+const API_KEY="5a4023ea121f5c51b9ed3c05d6fa0529";
 const API_URL="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 btn=document.querySelector(".search-btn button");
 userInput=document.querySelector(".search input");
 
+days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saterday']
+months=['Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec']
 
 async function checkWeather(city){
     const response=await fetch(API_URL + city + `&appid=${API_KEY}`);
     var data =await response.json();
-    
+    date=new Date();
     console.log(data);
-    console.log(data.message);
+    console.log(date.getDate());
     document.querySelector(".current-city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + '°c';
     document.querySelector(".current-weather").innerHTML=data.weather[0].description.slice(0,1).toUpperCase() + data.weather[0].description.slice(1);
     document.querySelector(".sub-content").innerHTML="Feels like " +Math.round(data.main.feels_like) + '°c';
-
+    document.querySelector(".current-date").innerHTML=`${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} at 4:57 AM`
     
 
     document.querySelector(".humidity .value").innerHTML=data.main.humidity + " %";
